f82d22da7379917599e2bc7e9ad033359fab6e73
LghtE <ehsenf7@gmail.com>
2025-01-17 18:49:32 +0530

    Init

---
+++ Scenes/Characters/BaseHunter/character_state_machine.gd
@@ -0,0 +1,11 @@
+extends Node
+
+enum States {IDLE, WALKING, RUNNING}
+
+@export var current_state : States = States.IDLE
+
+
+func _ready() -> void:
+	pass
+func _process(delta: float) -> void:
+	pass
