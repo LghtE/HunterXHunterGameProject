a2bc978ad659619559deb271d0ff3b631898ec59
LghtE <ehsenf7@gmail.com>
2025-01-25 02:43:05 +0530

    outline

--- Scenes/Characters/BaseHunter/FiniteStateMachine/turn.gd
+++ Scenes/Characters/BaseHunter/FiniteStateMachine/turn.gd
@@ -4,7 +4,7 @@ var need_to_turn = false
 func do(delta: float):
 	%Movement.player_movement_in_control = false
 	%Movement._rootNode.velocity = lerp(%Movement._rootNode.velocity, Vector3.ZERO, 0.1)
-	%AnimationPlayer.play("Turn")
+	%AnimationPlayer.play("Movement/Turn")
 
 func turnDone():
 	%Movement.player_movement_in_control = true
